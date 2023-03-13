7034b9825999176f298f9cb321b8bea860287ae4
Sebastian Camacho <sebbas192@gmail.com>
2023-03-13 16:30:04 -0500

    agregue los css, html, js

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,42 @@
+window.sr = ScrollReveal();
+    sr.reveal('.contenido1', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.contenido2', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.contenido3', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.logocontenido', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.logocontenido2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.encabezado', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-1000px'
+    });
+    
+    sr.reveal('.piepagina', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-1000px'
+    });
---
+++ src/main/resources/static/js/menu-lateral.js
@@ -0,0 +1,11 @@
+/*MENU LATERAL*/ 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function w3_open() {
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+/*FIN MENU LATERAL*/ 
