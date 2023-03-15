a72bfcb129249bdb6f991177bd8d25ac47d38e34
joansteven2003 <115471951+joansteven2003@users.noreply.github.com>
2023-03-15 09:20:34 -0500

    modificacion de diseño

--- src/main/java/com/app/web/controllers/IndexController.java
+++ src/main/java/com/app/web/controllers/IndexController.java
@@ -43,6 +43,6 @@ public class IndexController {
 	@GetMapping("/Registrarse")
 	public String Registrarse(Model modelo) {
 
-		return "/Registrarse.html";
+		return "/Registrarse";
 	}
 }
--- src/main/java/com/app/web/controllers/UsuarioController.java
+++ src/main/java/com/app/web/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.app.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.web.modelos.Usuario;
 import com.app.web.servicios.UsuarioServicio;
-
+@Controller
 public class UsuarioController {
 
 	@Autowired
