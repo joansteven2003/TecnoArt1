d6f5526bf288ab48fed6c341dc542a609e5d0744
joansteven2003 <115471951+joansteven2003@users.noreply.github.com>
2023-03-16 12:40:19 -0500

    mover archivos a base

---
+++ src/main/java/com/app/web/repositorio/HojaDeVidaRepositorio.java
@@ -0,0 +1,9 @@
+package com.app.web.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.web.modelos.HojaDeVida;
+
+public interface HojaDeVidaRepositorio extends JpaRepository<HojaDeVida, Long> {
+
+}
